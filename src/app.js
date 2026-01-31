d6971cd57940c8c8e9cf298edc3216291d3471fd
.... <shahinsamiur@yahoo.com>
2026-01-31 21:27:31 +0600

    first commit

---
+++ src/app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const cors = require("cors");
+const errorHandler = require("./middleware/errorHandler");
+
+const app = express();
+
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "http://192.168.0.105:3000"],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("right endpoint");
+});
+
+// app.use("/api/auth", authRoutes);
+
+app.use(errorHandler);
+
+module.exports = app;
